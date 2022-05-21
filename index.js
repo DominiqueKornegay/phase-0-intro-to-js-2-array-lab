5409c67c59120db952a499c5361377d8f2b897f2
Dominique Kornegay <dominiquekornegay@gmail.com>
2022-05-21 11:50:37 -0400

    commit tree 4251f403ba53e3be250dd69df6e37a41739bc661

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyCat = [...cats, name];
+    return copyCat;
+}
+
+function prependCat(name) {
+    const copyCat1 = [name, ...cats]
+    return copyCat1;
+}
+
+function removeLastCat() {
+    const copyCat2 = cats.slice(0, -1)
+    return copyCat2
+}
+
+
+function removeFirstCat() {
+    const copyCat3 = cats.slice(1)
+    return copyCat3
+}
